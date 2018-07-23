ff2cfaf87648a804075b75714c65102197f55c65
Daniel Boles <dawboles@ucsc.edu>
2018-07-22 17:36:51 -0700

    Beginning Unity Dev.

---
+++ First3DGame/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+
+	public Rigidbody ball;
+	int moveX;
+	int moveY;
+	int moveZ;
+
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Begin\n");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		ball.AddForce(0 + moveX, 0 + moveY, 0 + moveZ * Time.deltaTime);
+	}
+}
